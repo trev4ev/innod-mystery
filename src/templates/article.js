3fdf84e85e4db432f3f44ae79e1076fed61a2a77
trev4ev <trevoraquino@gmail.com>
2018-03-28 01:07:37 -0700

    change page path

--- src/templates/article.js
+++ src/templates/article.js
@@ -6,6 +6,7 @@ class ArticleTemplate extends React.Component {
         return (
             <div>
                 <h1>{blogPost.title}</h1>
+                <img src={blogPost.coverPhoto ? blogPost.coverPhoto.file.url : ''}  />
                 <h3>By {blogPost.author} </h3>
                 <h3>{blogPost.date}</h3>
                 <p>{blogPost.article.article}</p>
